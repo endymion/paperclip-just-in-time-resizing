358648128b63ecb4d17649c479b46130d5d336f1
Ryan Alyn Porter <rap@endymion.com>
2012-06-07 11:17:23 -0400

    Added support for uploading and displaying an image attachment to the scaffolding for the Image model.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,10 @@
 class Image < ActiveRecord::Base
-  # attr_accessible :title, :body
+  has_attached_file :attachment,
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  attr_accessible :attachment
 end
