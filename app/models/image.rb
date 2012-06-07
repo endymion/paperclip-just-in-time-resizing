131dfd2243623f5870e0d3aa6307f8573ece8b92
Ryan Alyn Porter <rap@endymion.com>
2012-06-07 12:16:24 -0400

    Added just-in-time dynamic thumbnail capability.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class Image < ActiveRecord::Base
   has_attached_file :attachment,
     :storage => :s3,
@@ -5,6 +7,27 @@ class Image < ActiveRecord::Base
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :styles => Proc.new { |attachment| attachment.instance.styles }
   attr_accessible :attachment
+  
+  @dynamic_style_format = ''
+  def dynamic_style_format_symbol
+    URI.escape(@dynamic_style_format).to_sym
+  end
+  
+  def styles
+    unless @dynamic_style_format.blank?
+      { dynamic_style_format_symbol => @dynamic_style_format }
+    else
+      {}
+    end
+  end
+
+  def dynamic_attachment_url(format)
+    @dynamic_style_format = format
+    attachment.reprocess!(dynamic_style_format_symbol) unless attachment.exists?(dynamic_style_format_symbol)
+    attachment.url(dynamic_style_format_symbol)
+  end
+  
 end
