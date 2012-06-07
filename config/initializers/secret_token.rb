0a0b2babfbfdb6fc1ea4e1201b6fe334169a9b5a
Ryan Alyn Porter <rap@endymion.com>
2012-06-07 10:48:59 -0400

    Generated Rails 3.2.5 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipJustInTimeResizing::Application.config.secret_token = '1aa78007c1809e9296718f64c7553386f893d5599c3dfa638ae4fc4b08b47a28640b34d97a9aac109e9ff01dcac4b6593e02249ac3f28a71e9616d9f5dc8e0c3'
